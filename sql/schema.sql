9bcfe8d3dace4a5d9883085357df48bd54fcaeaf
Stevephe123 <120182644+Stevephe123@users.noreply.github.com>
2026-01-11 21:29:53 +0800

    Add database schema and seed data

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS inventory_system;
+USE inventory_system;
+/*User Table*/
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP      
+);
+/*Category Table*/
+CREATE TABLE categories {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+/*Item Table*/
+CREATE TABLE items{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    category_id INT NOT NULL
+    quantity INT NOT NULL CHECK (quantity >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id) 
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+};
