0aceacd6e3717bfcffdfe54d2eca3d70d5d0e485
Stevephe123 <120182644+Stevephe123@users.noreply.github.com>
2026-01-11 23:08:48 +0800

    Update schema.sql
    fixing the sql data table problem

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,21 +9,21 @@ CREATE TABLE users(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP      
 );
 /*Category Table*/
-CREATE TABLE categories {
+CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-};
+);
 /*Item Table*/
-CREATE TABLE items{
+CREATE TABLE IF NOT EXISTS items(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    category_id INT NOT NULL
-    quantity INT NOT NULL CHECK (quantity >= 0),
+    category_id INT NOT NULL,
+    quantity INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES categories(id) 
     ON DELETE RESTRICT
     ON UPDATE CASCADE
-};
+);
